7d80633adb3f04cfed61b3ca289afc19f8012269
Greg Field <greg.field1992@gmail.com>
2023-10-26 21:43:45 +0100

    make dashboard component

--- app/(components)/DashBoard.jsx
+++ app/(components)/DashBoard.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import TicketCard from "./TicketCard";
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store",
+    });
 
-const DashBoard = () => {
+    return res.json();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const DashBoard = async () => {
+  const { tickets } = await getTickets();
+  console.log(tickets);
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category)),
+  ];
+
   return (
-    <div>DashBoard</div>
-  )
-}
-
-export default DashBoard
+    <div className="p-5">
+      <div>
+        {tickets &&
+          uniqueCategories.map((uniqueCategory, categoryIndex) => (
+            <div key={categoryIndex} className="mb-4">
+              <h2>{uniqueCategory}</h2>
+              <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+                {tickets
+                  .filter((ticket) => ticket.category === uniqueCategory)
+                  .map((filteredticket, _index) => (
+                    <TicketCard
+                      id={_index}
+                      key={_index}
+                      ticket={filteredticket}
+                    />
+                  ))}
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+export default DashBoard;
--- app/page.js
+++ app/page.js
@@ -1,47 +1,7 @@
-import TicketCard from "./(components)/TicketCard";
-const getTickets = async () => {
-  try {
-    const res = await fetch("http://localhost:3000/api/Tickets", {
-      cache: "no-store",
-    });
-  
+import Dashboard from "./(components)/DashBoard";
 
-    return res.json();;
-  } catch (err) {
-    console.error(err);
-  }
+const HomePage = async () => {
+  return <Dashboard />;
 };
 
-const Dashboard = async () => {
-  const {tickets} = await getTickets();
-  console.log(tickets);
-  const uniqueCategories = [
-    ...new Set(tickets?.map(({ category }) => category)),
-  ];
-
-  return (
-    <div className="p-5">
-      <div>
-        {tickets &&
-          uniqueCategories.map((uniqueCategory, categoryIndex) => (
-            <div key={categoryIndex} className="mb-4">
-              <h2>{uniqueCategory}</h2>
-              <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-                {tickets
-                  .filter((ticket) => ticket.category === uniqueCategory)
-                  .map((filteredticket, _index) => (
-                    <TicketCard
-                      id={_index}
-                      key={_index}
-                      ticket={filteredticket}
-                    />
-                  ))}
-              </div>
-            </div>
-          ))}
-      </div>
-    </div>
-  );
-};
-
-export default Dashboard;
+export default HomePage;
