9a5cd04a60ac7259b1aeb0b4519e3cf68823c3e7
Greg Field <greg.field1992@gmail.com>
2023-10-26 22:00:28 +0100

    .

--- app/(components)/DashBoard.jsx
+++ app/(components)/DashBoard.jsx
@@ -1,7 +1,7 @@
 import TicketCard from "./TicketCard";
 const getTickets = async () => {
   try {
-    let host = process.env.VERCEL_URL || "http://localhost:3000";
+    let host = "http://" + (process.env.VERCEL_URL || "localhost:3000");
 
     const res = await fetch(host + "/api/Tickets", {
       cache: "no-store",
